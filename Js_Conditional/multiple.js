d9823ab727271744a6c42bd32d5703d5e9af1ff8
nahid-hasan-dev <nahidhasanbd603@gmail.>
2025-10-11 15:26:49 +0600

    js shikhtechi

---
+++ Js_Conditional/multiple.js
@@ -0,0 +1,31 @@
+const salary = 25000;
+const isBCS = true;
+const height = 55;
+const hasCar = true;
+
+
+
+if(salary > 20000 && height > 57){
+    console.log('su---patro')
+}
+else{
+    console.log('onno patro khuji')
+}
+
+if(salary > 25000 || height > 54 || isBCS == true
+){
+    console.log('aso baba kobul')
+}
+else{
+    console.log('vag tui mokbul')
+}
+
+// -----------COMPLEX CONDITION----------
+
+
+if((salary > 25000 || hasCar == true) && isBCS == true){
+    console.log('tomar 14 ghosthi raji')
+}
+else{
+    console.log('raji na')
+}
