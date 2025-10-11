f608d651e8638ede79e6025783188a326a4816e5
nahid-hasan-dev <nahidhasanbd603@gmail.>
2025-10-11 15:01:12 +0600

    js shiktechi

---
+++ Js_Conditional/if.js
@@ -0,0 +1,41 @@
+/**
+ * tui jodi amake khaoyas 
+ *  taile ami asbo
+ * 
+ * ---------------
+ * if you buy food for me 
+ *        i will come 
+ * 
+ * -----------
+ * if biriyani price less than 300 taka 
+ *        i will buy
+ * 
+ * 
+ * -----STRUCTURE--------
+ * if (condition){
+ * 
+ * }
+ */
+
+if(3 < 10){
+    console.log('I have a smaller value')
+}
+
+if(13 < 10){
+    console.log('I have a smaller value')
+}
+
+var biriyani = 250;
+
+if(biriyani < 300){
+    console.log('mama biriyani dao')
+}
+
+var weight = 20;
+
+if(weight < 40){
+    console.log('I will rent a rikshaw')
+}
+else{
+    console.log('I will carry it by myself')
+}
