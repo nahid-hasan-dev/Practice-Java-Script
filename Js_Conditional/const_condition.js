b7150f602ff652401154fff3af963c7cb68a23d7
nahid-hasan-dev <nahidhasanbd603@gmail.>
2025-10-11 15:44:10 +0600

    js shikhtechi

---
+++ Js_Conditional/const_condition.js
@@ -0,0 +1,17 @@
+const salary = 25000;
+const isBCS = true;
+const height = 57;
+
+if(salary > 20000 && height > 56){
+    console.log('su---------patro')
+}
+else{
+    console.log('onno patro khuji')
+}
+
+if(salary > 25000 || height > 54){
+    console.log('eso baba kobul')
+}
+else{
+    console.log('vag tui mikbul')
+}
